19af7d9f12807d25fe3ba0d8a38f749296537b07
Richard150 <46606377+Richard150@users.noreply.github.com>
2020-11-22 22:32:06 -0500

    new account logic

--- server/Account.js
+++ server/Account.js
@@ -2,11 +2,21 @@ const account_data = require('data-store')({path: process.cwd() + '/data/account
 
 class Account {
 
-    constructor(username, password, wins, losses) {
+    constructor(username, password, avatar) {
         this.username = username;
         this.password = password;
-        this.wins = wins;
-        this.losses = losses;
+        this.avatar = avatar;
+        this.wins = 0;
+        this.losses = 0;
+        this.successfulAttacks = 0;
+        this.failedAttacks = 0;
+        this.successfulBlocks = 0;
+        this.failedBlocks = 0;
+        this.successfulCounters = 0;
+        this.failedCounters = 0;
+        this.successfulHeals = 0;
+        this.failedHeals = 0;
+        this.totalRepairs = 0;
     }
 
     update() {
@@ -21,23 +31,165 @@ class Account {
 
 Account.userExists = (username) => {
     let adata = account_data.get(username);
-    if (adata == null) {
+    if (adata == undefined) {
         return false;
     } else {
         return true;
     }
 }
 
+Account.setAvatar = (username, avatar) => {
+    if (avatar > 41 || avatar < 0) { return undefined }
+    
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.avatar = avatar;
+    account_data.set(adata.username, adata);
+    return true;
+}
+
+Account.addSucAttack = (username) => {
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.successfulAttacks++;
+    account_data.set(adata.username, adata);
+    return adata.successfulAttacks;
+}
+
+Account.addFailedAttack = (username) => {
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.failedAttacks++;
+    account_data.set(adata.username, adata);
+    return adata.failedAttacks;
+}
+
+Account.addSucBlock = (username) => {
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.successfulBlocks++;
+    account_data.set(adata.username, adata);
+    return adata.successfulBlocks;
+}
+
+Account.addFailedBlock = (username) => {
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.failedBlocks++;
+    account_data.set(adata.username, adata);
+    return adata.failedBlocks;
+}
+
+Account.addSucCounters = (username) => {
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.successfulCounters++;
+    account_data.set(adata.username, adata);
+    return adata.successfulCounters;
+}
+
+Account.addFailedCounters = (username) => {
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.failedCounters++;
+    account_data.set(adata.username, adata);
+    return adata.failedCounters;
+}
+
+Account.addSucHeals = (username) => {
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.successfulHeals++;
+    account_data.set(adata.username, adata);
+    return adata.successfulHeals;
+}
+
+Account.addFailedHeals = (username) => {
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.failedHeals++;
+    account_data.set(adata.username, adata);
+    return adata.failedHeals;
+}
+
+Account.addRepair = (username) => {
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.totalRepairs++;
+    account_data.set(adata.username, adata);
+    return adata.totalRepairs;
+}
+
+Account.addWin = (username) => {
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.wins++;
+    account_data.set(adata.username, adata);
+    return adata.wins;
+}
+
+Account.addLoss = (username) =>{
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    adata.losses++;
+    account_data.set(adata.username, adata);
+    return adata.losses;
+}
+
+Account.getAvatar = (username) => {
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    return adata.avatar;
+}
+
 Account.getPassword = (username) => {
-    return account_data.get(username).password;
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    return adata.password;
 }
 
 Account.getWins = (username) => {
-    return account_data.get(username).wins;
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    return adata.wins;
 }
 
 Account.getLosses = (username) => {
-    return account_data.get(username).losses;
+    let adata = account_data.get(username);
+    if (adata == undefined) {
+        return undefined;
+    }
+    return adata.losses;
 }
 
 Account.getAllUsers = () => {
@@ -47,19 +199,21 @@ Account.getAllUsers = () => {
 
 Account.findByUsername = (username) => {
     let adata = account_data.get(username);
-    if (adata == null) {
-        return null;
+    if (adata == undefined) {
+        return undefined;
     }
-    return new Account(adata.username, adata.password, adata.wins, adata.losses);
+    return new Account(adata.username, adata.password, adata.avatar, adata.wins, adata.losses);
 }
 
-Account.create = (username, password) => {
-    if (Object.keys(account_data.data).filter(nextUser => nextUser == username)) {
-        return null;
+Account.create = (username, password, avatar) => {
+    if (Object.keys(account_data.data).filter(nextUser => nextUser == username).length > 0) {
+        return undefined;
     }
-    let a = new Account(username, password, 0, 0);
+    let a = new Account(username, password, avatar);
     account_data.set(a.username, a);
     return a;
 }
+
+console.log(Account.setAvatar('richyj', 33));
 
 module.exports = Account;
