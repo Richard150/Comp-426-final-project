9df30024b16983789a7e0bb859a8b0093e3c5133
unknown <noah.vh98@gmail.com>
2020-11-23 19:08:18 -0500

    only accept alphanumeric names <20chars

--- server/index.js
+++ server/index.js
@@ -135,7 +135,9 @@ io.on('connection', (socket) => {
             let username = credentials.username;
             let password = credentials.password;
 
-            if(forbiddenNames.includes(username) || Account.userExists(username)) {
+            cleanName = name.replace(/\W/g,'').substring(0,20);
+
+            if(cleanName != name || forbiddenNames.includes(username) || Account.userExists(username)) {
                 socket.emit('signup unsuccessful');
             } else {
                 socket.emit('signup successful');
